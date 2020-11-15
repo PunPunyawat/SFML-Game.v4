f9712932f86d33b74bad7ed18e6ca09666cec3ba
PunPunyawat <69188795+PunPunyawat@users.noreply.github.com>
2020-11-15 17:12:48 +0700

    Add files via upload

---
+++ Menu.cpp
@@ -0,0 +1,57 @@
+﻿#include "Menu.h"
+using namespace sf;
+
+Menu::Menu(float width, float height)
+{
+	if (!font.loadFromFile("font/Jelly Crazies.ttf"))
+	{
+		// handle error
+	}
+
+	menu[0].setFont(font);
+	//menu[0].setColor( sf::Color::Red );
+	menu[0].setString("PLAY");			//width อันตราส่วนความกว้าง    // ความสูง    // * 1 ให้อยุ่บนสุด
+	menu[0].setPosition(sf::Vector2f(width / 3, height / (MAX_NUMBER_OF_ITEMS + 1) * 1));
+
+	menu[1].setFont(font);
+	//menu[1].setColor(sf::Color::White );
+	menu[1].setString("OPTIONS");
+	menu[1].setPosition(sf::Vector2f(width / 3, height / (MAX_NUMBER_OF_ITEMS + 1) * 2));
+
+	menu[2].setFont(font);
+	//menu[2].setColor( sf::Color::White);
+	menu[2].setString("EXIT");
+	menu[2].setPosition(sf::Vector2f(width / 3, height / (MAX_NUMBER_OF_ITEMS + 1) * 3));
+
+
+	selectedItemIndex = 0;
+
+
+}
+
+Menu::~Menu()
+{
+}
+
+
+void Menu::draw(sf::RenderWindow &window)
+{
+	for (int i = 0; i < MAX_NUMBER_OF_ITEMS; i++)
+	{
+		window.draw(menu[i]);
+	}
+
+
+}
+
+void Menu::MoveUp()
+{
+
+
+}
+
+void Menu::MoveDown()
+{
+
+
+}
